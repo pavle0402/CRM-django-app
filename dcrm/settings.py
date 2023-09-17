b2f4d787306c154633ead0dc67ac22b42a25469c
Pavle Stojicevic <pavles2002@gmail.com>
2023-09-17 23:35:10 +0200

    change

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
+STATIC_ROOT = 'temp-static-root/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
                     )
