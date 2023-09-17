41aead5bac83a93935552f9cedc53ac6a418b834
Pavle Stojicevic <pavles2002@gmail.com>
2023-09-17 23:19:52 +0200

    static root update

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = None
+STATIC_ROOT = ''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
                     )
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
